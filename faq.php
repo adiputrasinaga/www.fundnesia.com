e951e40ca3be411d66d36d69a99d7e520b6391ae
Adiputra Sinaga <32774236+adiputrasinaga@users.noreply.github.com>
2019-08-08 12:47:46 +0700

    Update faq.php

--- faq.php
+++ faq.php
@@ -33,20 +33,20 @@ include $_SESSION['lang'] . '.php';
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="generator" content="Mobirise v4.6.5, mobirise.com">
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-  <link rel="shortcut icon" href="assets/images/logo-single.png" type="image/x-icon">
+  <link rel="shortcut icon" href="images/logo-single.png" type="image/x-icon">
   <meta name="description" content="Site Builder Description">
   <title><?php echo $lang_judul; ?></title>
-  <link rel="stylesheet" href="assets/web/assets/mobirise-icons-bold/mobirise-icons-bold.css">
-  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
-  <link rel="stylesheet" href="assets/tether/tether.min.css">
-  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
-  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
-  <link rel="stylesheet" href="assets/dropdown/css/style.css">
-  <link rel="stylesheet" href="assets/socicon/css/styles.css">
-  <link rel="stylesheet" href="assets/animatecss/animate.min.css">
-  <link rel="stylesheet" href="assets/theme/css/style.css">
-  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  <link rel="stylesheet" href="web/assets/mobirise-icons-bold/mobirise-icons-bold.css">
+  <link rel="stylesheet" href="web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="tether/tether.min.css">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="dropdown/css/style.css">
+  <link rel="stylesheet" href="socicon/css/styles.css">
+  <link rel="stylesheet" href="animatecss/animate.min.css">
+  <link rel="stylesheet" href="theme/css/style.css">
+  <link rel="stylesheet" href="mobirise/css/mbr-additional.css" type="text/css">
   
   
   
@@ -69,7 +69,7 @@ include $_SESSION['lang'] . '.php';
             <div class="navbar-brand">
                 <span class="navbar-logo">
                     <a href="https://fundnesia.com">
-                        <img src="assets/images/fundnesia-logo-5-960x560.png" alt="Fundnesia" title="" style="height: 4.1rem;">
+                        <img src="images/fundnesia-logo-5-960x560.png" alt="Fundnesia" title="" style="height: 4.1rem;">
                     </a>
                 </span>
                 
@@ -413,17 +413,17 @@ include $_SESSION['lang'] . '.php';
 </section>
 
 
-  <script src="assets/web/assets/jquery/jquery.min.js"></script>
-  <script src="assets/popper/popper.min.js"></script>
-  <script src="assets/tether/tether.min.js"></script>
-  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-  <script src="assets/smoothscroll/smooth-scroll.js"></script>
-  <script src="assets/dropdown/js/script.min.js"></script>
-  <script src="assets/viewportchecker/jquery.viewportchecker.js"></script>
-  <script src="assets/mbr-switch-arrow/mbr-switch-arrow.js"></script>
-  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
-  <script src="assets/theme/js/script.js"></script>
-  <script src="assets/formoid/formoid.min.js"></script>
+  <script src="web/assets/jquery/jquery.min.js"></script>
+  <script src="popper/popper.min.js"></script>
+  <script src="tether/tether.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script src="smoothscroll/smooth-scroll.js"></script>
+  <script src="dropdown/js/script.min.js"></script>
+  <script src="viewportchecker/jquery.viewportchecker.js"></script>
+  <script src="mbr-switch-arrow/mbr-switch-arrow.js"></script>
+  <script src="touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="theme/js/script.js"></script>
+  <script src="formoid/formoid.min.js"></script>
   
   
  <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i></i></a></div>
